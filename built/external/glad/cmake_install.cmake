a05573946c8d38fe5aa85a3f689a2f7bc48404dc
Vladislav <62977jsa1@gmail.com>
2022-05-29 13:46:13 +0300

    не знаю что произошло

--- built/external/glad/cmake_install.cmake
+++ built/external/glad/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/KEK/rep/Seriozniy
+# Install script for directory: C:/KEK/rep/Seriozniy/external/glad
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,20 +32,3 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
 endif()
 
-if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("C:/KEK/rep/Seriozniy/built/external/glfw/cmake_install.cmake")
-  include("C:/KEK/rep/Seriozniy/built/external/glad/cmake_install.cmake")
-
-endif()
-
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/KEK/rep/Seriozniy/built/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
